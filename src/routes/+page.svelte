5a8eb35aefe4d5f3a576f40b2580e9f4a935b344
Joel <58995538+jtpotato@users.noreply.github.com>
2023-09-25 09:43:45 +1000

    i like sveltekit now

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+    let gameLength = 8
+    let clicksRemaining = 8
+	let clickTimes = [0];
+
+	const handleClick = () => {
+		clickTimes.push(window.performance.now());
+        clicksRemaining--;
+		console.log(clickTimes);
+        checkIfGameIsOver();
+	};
+
+    const checkIfGameIsOver = () => {
+        if (clicksRemaining === 0) {
+            goto(`/stats?clickTimes=${clickTimes}&gameLength=${gameLength}`);
+        }
+    }
+</script>
+
+<button on:click={handleClick}>Tap me!</button>
+<p>Tap {clicksRemaining} times</p>
---
+++ src/routes/stats/+page.svelte
@@ -0,0 +1,10 @@
+<!--Test with this URL: http://localhost:5173/stats?clickTimes=0,125480,126015,126620,127227,127848,128505,129164.99999999999,129771.00000000001&gameLength=8 -->
+
+<script>
+    import { page } from "$app/stores";
+    console.log($page.url.searchParams.get("clickTimes"))
+</script>
+
+<p>These are the analytics</p>
+<p>Game length: {$page.url.searchParams.get("gameLength")}</p>
+<p>Click times: {$page.url.searchParams.get("clickTimes")}</p>
