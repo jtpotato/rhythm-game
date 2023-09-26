277a5a36343b23de08f9d83b6e76ad07b15f0fa7
Joel <58995538+jtpotato@users.noreply.github.com>
2023-09-26 23:10:14 +1000

    have ssr disabled be an option

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.postcss';
 	import '@carbon/charts-svelte/styles.css';
+
+	// export const ssr = false; sorry we dont need SEO
 </script>
 
 <div class="flex p-12 max-w-6xl mx-auto min-h-screen">
